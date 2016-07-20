76a35b0c7fe60bdfbd452d75302ebe1583593bd7
Lorna <lornanqodi@yahoo.com>
2016-07-20 18:31:48 +0200

    Latest Updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var GOOGLE_MAPS_API_KEY = "AIzaSyAqC35pe9DRE5Tr883yw_jy22PbsLo6eYI";
 
-var app = angular.module('escapeApp', ['ngRoute']);
+var app = angular.module('escapeApp', ['ngRoute','ngMaps']);
 
 app.config(function($routeProvider) {
 	$routeProvider.when('/', {
@@ -21,6 +21,19 @@ app.controller('MarketingCtrl', function($scope) {
 app.controller('TripsCtrl', function($scope) {
 });
 
-app.controller('ExploreCtrl', function($scope) {
-
+app.controller('ExploreCtrl', function($scope, $http, $route) {
+	$http({
+		url: "http://ec2-52-33-4-120.us-west-2.compute.amazonaws.com:8000/hello",
+		method:"POST",
+		params: {
+			subtypes: "shopping", 
+			lat:18.41, 
+			long:-33.910,
+		},
+		}).then(function(response){
+			console.log(response.data);
+			
+		}).catch(function(response) {
+      		$scope.error = response.data.message;
+    	});
 });
