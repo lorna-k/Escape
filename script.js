a5c01d77c04c9a9a2b9eb80ecdf78249a9b4623a
Isabel Benatar <isabelbenatar@Isabels-MacBook-Air.local>
2016-07-20 13:46:12 +0200

    added angular

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 var GOOGLE_MAPS_API_KEY = "AIzaSyAqC35pe9DRE5Tr883yw_jy22PbsLo6eYI";
+
+var app = angular.module('escapeApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		controller: 'MarketingCtrl',
+		templateUrl: '/templates/marketing.html'
+	}).when('/mytrips', {
+		controller: 'MytripsCtrl',
+		templateUrl: '/templates/mytrips.html'
+	})
+});
+
+app.controller('MarketingCtrl', function ($scope) { 
+});
+
+app.controller('MytripsCtrl', function($scope) {
+});
