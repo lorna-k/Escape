d651200b56bae00fb4f1c8eda6bb54a9a1c9fec6
Daniel Nathan <danielnathan97@gmail.com>
2016-07-20 21:45:54 +0200

    some changes

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ app.controller('ExploreCtrl', function($scope, $http, $route) {
 app.controller('Main', function($scope) {
 
       $scope.map = {
-  center: [47.5, -122.5],
+  center: [-33.924, 18.424],
   options: function() {
       return {
         streetViewControl: false,
